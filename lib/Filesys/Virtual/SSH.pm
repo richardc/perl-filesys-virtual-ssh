b020a09d05b4a07bc6c639519f5dd4df7e393e02
richardc <richardc@c56e198b-7db7-0310-b3ab-8e406f684e33>
2004-04-08 10:52:54 +0000

    tramp beater?
    git-svn-id: file:///web/svn.unixbeard.net/repo/richardc/perl/Filesys-Virtual-SSH@2344 c56e198b-7db7-0310-b3ab-8e406f684e33

---
+++ lib/Filesys/Virtual/SSH.pm
@@ -0,0 +1,51 @@
+package Filesys::Virtual::SSH;
+
+use strict;
+#use warnings;
+
+
+
+=head1 NAME
+
+Filesys::Virtual::SSH - 
+
+=head1 SYNOPSIS
+
+  use Filesys::Virtual::SSH;
+
+
+
+=head1 DESCRIPTION
+
+
+
+=cut
+
+
+
+=head1 AUTHOR
+
+Richard Clamp <richardc@unixbeard.net>
+
+=head1 COPYRIGHT
+
+Copyright 2004 Richard Clamp.  All Rights Reserved.
+
+This program is free software; you can redistribute it
+and/or modify it under the same terms as Perl itself.
+
+
+=head1 BUGS
+
+None known.
+
+Bugs should be reported to me via the CPAN RT system.
+L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Filesys::Virtual::SSH>.
+
+
+=head1 SEE ALSO
+
+
+=cut
+
+1;
---
+++ t/vs_plain.t
@@ -0,0 +1,10 @@
+#!perl
+use strict;
+use warnings;
+use Test::More tests => 1;
+use Filesys::Virtual::Plain;
+use Filesys::Virtual::SSH;
+
+# A comparitive test against Filesys::Virtual::Plain, as Filesys::Virtual module is low on docs
+
+ok(1);
